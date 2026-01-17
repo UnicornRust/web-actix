c604f6082c9d39655a2193a7b95fd054e304f435
ancion <ancion@github.com>
2026-01-18 03:06:24 +0800

    update

--- webservice/src/routers.rs
+++ webservice/src/routers.rs
@@ -18,11 +18,12 @@ use crate::handlers::teacher::{
 
 pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
-}
+} 
+
 
 pub fn course_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
-web::scope("/courses")
+        web::scope("/courses")
             .route("/", web::post().to(post_new_course))
             .route("/{teacher_id}", web::get().to(get_courses_for_teacher))
             .route("/{teacher_id}/{course_id}", web::get().to(get_course_detail))
@@ -33,11 +34,11 @@ web::scope("/courses")
 
 pub fn teacher_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
-web::scope("/teachers")
+        web::scope("/teachers")
             .route("/", web::post().to(post_new_teacher))
             .route("/", web::get().to(get_all_teacher))
             .route("/{teacher_id}", web::get().to(get_teacher_details))
             .route("/{teacher_id}",web::put().to(update_teacher_details))
             .route("/{teacher_id}", web::delete().to(delete_teacher))
     );
-            }
+}
